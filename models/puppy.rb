32b113f3d18f4210f3900daa87b15c3d2e35e64d
Reno Sorensen <renojames19@gmail.com>
2019-05-17 04:15:44 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,10 +4,10 @@ class Puppy
 
   attr_accessor :name, :breed, :months_old
 
-  def initialize(name, breed, months_old)
-    @name = name
-    @breed = breed
-    @months_old = months_old
+  def initialize(params)
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:months_old]
   end
 
 end
